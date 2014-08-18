1b54c880362e57f38f8e117d50bbe33531a138f2
Jason Buckner <jason@jasonbuckner.com>
2014-08-17 21:27:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Thumbnailer
+
+A script that takes a directory of images, resizes them to their longest side and adds borders to fill the space if they don't fit.
+
+### Configuration:
+
+Edit `thumbnailer.py` and set the width, height, background color, source, and target directories.
+
+### Usage:
+
+```
+$ python thumbnailer.py
+```
+
+### Why?
+
+I took *a lot* of pictures at the Golden Gate Relay and wanted to [make a video](https://www.youtube.com/watch?v=aACiyeQ9dFM) of them, but I needed all the pictures to match up.
---
+++ thumbnailer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import glob
+import os
+
+from PIL import Image
+from progressbar import ProgressBar
+
+width, height = 1024, 678  # in px
+r, g, b = 0, 0, 0  # background color, 0-255
+source_dir = 'ggr'
+targer_dir = '%s_%sx%s' % (source_dir, width, height)
+
+try:
+    os.makedirs(targer_dir)
+except:
+    pass
+
+filelist = glob.glob('%s/*' % source_dir)
+number_of_files = len(filelist)
+
+count = 0
+progress = ProgressBar(maxval=number_of_files).start()
+for filename in filelist:
+    if filename[-3:].lower() not in ('png', 'jpg', 'gif'):
+        continue
+    image = Image.open('%s' % filename)
+
+    exif = image._getexif()
+    orientation_key = 274  # cf ExifTags
+    if orientation_key in exif:
+        orientation = exif[orientation_key]
+
+        rotate_values = {
+            3: 180,
+            6: 270,
+            8: 90
+        }
+
+        if orientation in rotate_values:
+            # Rotate and save the picture
+            image = image.rotate(rotate_values[orientation])
+
+    image.thumbnail((width, height), Image.ANTIALIAS)
+    background = Image.new('RGB', (width, height), (r, g, b))
+    background.paste(
+        image,
+        ((width - image.size[0]) / 2, (height - image.size[1]) / 2))
+    background.save('%s/%s' % (targer_dir, os.path.basename(filename)), 'JPEG')
+    count = count + 1
+    progress.update(count)
